7d5a23673ea01b4ba2076c737dfbf7f382d39bdd
Gxbito <g4bmz02@gmail.com>
2024-04-15 16:27:22 -0300

    datos agregados

---
+++ E-commerce/src/data/categories.js
@@ -0,0 +1,31 @@
+export const categories = [
+    {
+        id: 1,
+        title: "Dress Coat",
+        category: "DressCoat",
+    },
+
+    {
+        id: 2,
+        title: "Jacket",
+        category: "Jacket",
+    },
+
+    {
+        id: 3,
+        title: "Dress",
+        category: "Dress",
+    },
+
+    {
+        id: 4,
+        title: "T-shirt",
+        category: "T-shirt",
+    },
+
+    {
+        id: 5,
+        title: "Pants",
+        category: "Pants",
+    },
+]
