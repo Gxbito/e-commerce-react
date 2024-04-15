ed2b6d0272de4a91d69113ab256af323ea22283e
Gaby Matinez <g4bmz02@gmail.com>
2024-04-15 18:53:38 -0300

    datos actualizados

--- E-commerce/src/data/products.js
+++ E-commerce/src/data/products.js
@@ -44,14 +44,50 @@ export const products = [
         price: 120,
         image: "../assets/images/Clothing-Man-5.jpg"
     },
-    
+
     {
         id: 101,
         title: "titulo",
         category: "Jacket",
         description: "descripción",
         price: 120,
-        image: "../assets/images/Clothing-Man-5.jpg"
+        image: "../assets/images/Clothing-Woman-1.jpg"
+    },
+
+    {
+        id: 102,
+        title: "titulo",
+        category: "DressCoat",
+        description: "descripción",
+        price: 120,
+        image: "../assets/images/Clothing-Woman-2.jpg"
+    },
+
+    {
+        id: 103,
+        title: "titulo",
+        category: "Dress",
+        description: "descripción",
+        price: 120,
+        image: "../assets/images/Clothing-Woman-3.jpg"
+    },
+
+    {
+        id: 104,
+        title: "titulo",
+        category: "Dress",
+        description: "descripción",
+        price: 120,
+        image: "../assets/images/Clothing-Woman-4.jpg"
+    },
+
+    {
+        id: 105,
+        title: "titulo",
+        category: "Jacket",
+        description: "descripción",
+        price: 120,
+        image: "../assets/images/Clothing-Woman-5.jpg"
     },
 
 ]
