7e29fa7ef57a462bcc7260b638138260a495fdf0
Gxbito <g4bmz02@gmail.com>
2024-04-16 12:06:59 -0300

    estilos agregados

--- E-commerce/src/data/products.js
+++ E-commerce/src/data/products.js
@@ -1,93 +1,91 @@
 export const products = [
+  {
+    id: 1,
+    title: "titulo",
+    category: "DressCoat",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Man-1.jpg",
+  },
 
-    {
-        id: 1,
-        title: "titulo",
-        category: "DressCoat",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Man-1.jpg"
-    },
+  {
+    id: 2,
+    title: "titulo",
+    category: "Jacket",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Man-2.jpg",
+  },
 
-    {
-        id: 2,
-        title: "titulo",
-        category: "Jacket",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Man-2.jpg"
-    },
+  {
+    id: 3,
+    title: "titulo",
+    category: "Dress",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Man-3.jpg",
+  },
 
-    {
-        id: 3,
-        title: "titulo",
-        category: "Dress",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Man-3.jpg"
-    },
+  {
+    id: 4,
+    title: "titulo",
+    category: "DressCoat",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Man-4.jpg",
+  },
 
-    {
-        id: 4,
-        title: "titulo",
-        category: "DressCoat",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Man-4.jpg"
-    },
+  {
+    id: 5,
+    title: "titulo",
+    category: "Jacket",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Man-5.jpg",
+  },
 
-    {
-        id: 5,
-        title: "titulo",
-        category: "Jacket",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Man-5.jpg"
-    },
+  {
+    id: 101,
+    title: "titulo",
+    category: "Jacket",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Woman-1.jpg",
+  },
 
-    {
-        id: 101,
-        title: "titulo",
-        category: "Jacket",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Woman-1.jpg"
-    },
+  {
+    id: 102,
+    title: "titulo",
+    category: "DressCoat",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Woman-2.jpg",
+  },
 
-    {
-        id: 102,
-        title: "titulo",
-        category: "DressCoat",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Woman-2.jpg"
-    },
+  {
+    id: 103,
+    title: "titulo",
+    category: "Dress",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Woman-3.jpg",
+  },
 
-    {
-        id: 103,
-        title: "titulo",
-        category: "Dress",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Woman-3.jpg"
-    },
+  {
+    id: 104,
+    title: "titulo",
+    category: "Dress",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Woman-4.jpg",
+  },
 
-    {
-        id: 104,
-        title: "titulo",
-        category: "Dress",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Woman-4.jpg"
-    },
-
-    {
-        id: 105,
-        title: "titulo",
-        category: "Jacket",
-        description: "descripción",
-        price: 120,
-        image: "../assets/images/Clothing-Woman-5.jpg"
-    },
-
-]
+  {
+    id: 105,
+    title: "titulo",
+    category: "Jacket",
+    description: "descripción",
+    price: 120,
+    image: "../assets/images/Clothing-Woman-5.jpg",
+  },
+];
